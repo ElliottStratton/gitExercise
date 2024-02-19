5e6dcf8bf846c6153e2fe1e5feba133dced2d75d
ElliottStratton <elliottanthonystratton@gmail.com>
2024-02-19 14:29:49 -0500

    .gitignore update

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hey, it's me Elliott");
+        System.out.println("I would love to add this");
     }
 }
