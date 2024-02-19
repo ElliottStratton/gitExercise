e3c0fd0c1beb6bdcffc051dd63615ac997267bd1
ElliottStratton <elliottanthonystratton@gmail.com>
2024-02-19 14:36:42 -0500

    new line 2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hey, it's me Elliott");
         System.out.println("I would love to add this");
+        System.out.println();
     }
 }
